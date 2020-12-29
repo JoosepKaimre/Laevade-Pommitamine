7b1b2497d7ccead3b5ffad21f0ca62b4ca3a012b
Joosep Kaimre <joosepka@gmail.com>
2020-12-29 16:35:42 +0200

    lisatud hunti ja tõenäosusliku kombinatsioon, praegu see tõenäosuslik pole kõige parem

--- kood/algoritmid.py
+++ kood/algoritmid.py
@@ -92,4 +92,58 @@ def paarsus_käik(laud):
     # kui ei leidu ruutu, millel poleks mingi naaber pommitamata, siis pommitame viimase vaba ruudu
     return laua_meetodid.pommita(rida,veerg,laud)
             
-        
+def hunt_tõenäosuslik(laud):
+    global targad_käigud
+    global viimati_pommitatud
+    if len(targad_käigud) != 0:
+        shuffle(targad_käigud)
+        viimati_pommitatud = targad_käigud.pop()
+        tulem, laud = laua_meetodid.pommita(viimati_pommitatud[0],viimati_pommitatud[1],laud)
+        if tulem == 1:
+            kõrvuti_ruudud(viimati_pommitatud,laud)
+        return tulem, laud
+    else:
+        tulem, laud = tõenäosuslik(laud)
+        if tulem == 1:
+            kõrvuti_ruudud(viimati_pommitatud,laud)
+        return tulem, laud
+
+
+def tõenäosuslik(laud):
+    tühi_maatriks = [[0]*10]*10
+    laevad = [5,4,3,3,2]
+    # vasakult paremale kontrollid
+    for laev in laevad:
+        for rida in range(len(laud)):
+            for veerg in range(len(laud)+1-laev):
+                if tühjuse_kontroll_rida(rida,veerg,laev,laud):
+                    for i in range(laev):
+                        tühi_maatriks[rida][veerg+i] +=1
+                if tühjuse_kontroll_veerg(veerg,rida,laev,laud):
+                    for i in range(laev):
+                        tühi_maatriks[rida+1][veerg] +=1
+    max = -1
+    rida = -1
+    veerg = -1
+    for i in range(10):
+        for j in range(10):
+            if tühi_maatriks[i][j] > max:
+                rida = i
+                veerg = j
+    return laua_meetodid.pommita(rida,veerg,laud)
+                        
+                    
+    
+def tühjuse_kontroll_rida(rida,veerg,laev,laud):
+    for i in range(laev):
+        if (laud[rida][veerg+i] == "-" or laud[rida][veerg+i] == "X"):
+            return False
+    return True
+
+def tühjuse_kontroll_veerg(rida,veerg,laev,laud):
+    for i in range(laev):
+        if (laud[rida+i][veerg] == "-" or laud[rida+i][veerg] == "X"):
+            return False
+    return True
+                    
+    
